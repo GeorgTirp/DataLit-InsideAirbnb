3a759e80070d4d32347190047c110d9552f70d96
WizgallF <frieder.wizgall@gmail.com>
2025-01-19 22:49:51 +0100

    Added sphinx documentation to project

---
+++ source/Models.rst
@@ -0,0 +1,15 @@
+.. InsideAirBnB documentation master file, created by
+   sphinx-quickstart on Sun Jan 19 22:00:56 2025.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Models
+======
+
+XGBoost Regressor
+-----------------
+
+An XGBoost Regressor model trained using a nested cross-validation.
+Additionally the features were explained using Shapley additive explanations (SHAP).
+
+
---
+++ source/conf.py
@@ -0,0 +1,27 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'InsideAirBnB'
+copyright = '2025, Georg Tirpitz, Nils Klute, Matthis Nommensen, Frieder Wizgall'
+author = 'Georg Tirpitz, Nils Klute, Matthis Nommensen, Frieder Wizgall'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "sphinxawesome_theme"
+html_static_path = ['_static']
