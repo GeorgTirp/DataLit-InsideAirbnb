bbdc6bd00aa307f8dbc0d61de743f6456602eed2
Georg Tirpitz <georg.tirpitz@student.uni-tuebingen.de>
2025-01-21 20:04:14 +0100

    made a main that can iterate through a whole folder of cities (still in progress)

--- models/regression_main.py
+++ models/regression_main.py
@@ -7,13 +7,18 @@ import matplotlib.pyplot as plt
 import logging
 import os
 from add_custom_features import AddCustomFeatures
-from TabPFN_pipeline import TabPFNRegression
+from regression import RegressionModels
 
-def tab_pfn_main(folder_path,  file_path ,safe_path: str, identifier: str):
+def regression_main(folder_path,  file_path ,safe_path: str, identifier: str):
     #folder_path = "/Users/georgtirpitz/Documents/Data_Literacy"
     data_df = pd.read_csv(file_path)
     safe_path = folder_path + "/results" + "test"
     identifier = "tabpfn"
+    #folder_path = "/Users/georgtirpitz/Documents/Data_Literacy"
+    folder_path = "/home/georg/Documents/Master/Data_Literacy"
+    data_df = pd.read_csv(folder_path + "/city_listings.csv")
+    safe_path = folder_path + "/results" + "test"
+    identifier = ["RandomForest", "LinearRegression"]
     # Setting features and target
     Feature_Selection = {
         'features': [
@@ -33,12 +38,31 @@ def tab_pfn_main(folder_path,  file_path ,safe_path: str, identifier: str):
     add_custom_features = ['distance_to_city_center', 'average_review_length']
     Feature_Adder = AddCustomFeatures(data_df, add_custom_features)
     data_df = Feature_Adder.return_data()
-    model = TabPFNRegression(data_df, Feature_Selection, test_split_size, safe_path, identifier)
+    RandomForest_Hparams = {
+        'n_estimators': 100,
+        'max_depth': 10,
+        'random_state': 42
+    }
+    n_top_features = 15 # Number of top features to show
+
+    model = model = RegressionModels(
+        data_df, 
+        RandomForest_Hparams, 
+        Feature_Selection, 
+        test_split_size, 
+        safe_path, 
+        identifier, 
+        n_top_features)
     X, y = model.model_specific_preprocess(data_df, Feature_Selection)
+    # Setting Hyperparameters and features
+    
     model.fit()
-    model.predict(X, y, save_results=True)
-    model.evaluate()
-    model.feature_importance()
+    X, y = model.model_specific_preprocess(data_df, Feature_Selection)
+    preds = model.predict(X, y, save_results=True)
+    metrics = model.evaluate()
+    importances = model.feature_importance(10)
+    model.plot()
+    print("Finished")
 
     if __name__ == "__main__":
         folder_path = "/home/georg/Documents/Master/Data_Literacy"
@@ -51,6 +75,6 @@ def tab_pfn_main(folder_path,  file_path ,safe_path: str, identifier: str):
                 city_name = ""
                 file_path = "/home/georg/Documents/Master/Data_Literacy/city_listings.csv"
                 safe_path = "/home/georg/Documents/Master/Data_Literacy/results" + city_name
-                identifier = "tabpfn"
-                tab_pfn_main(folder_path, safe_path, identifier)
+                identifier = "regression"
+                regression_main(folder_path, safe_path, identifier)
 
