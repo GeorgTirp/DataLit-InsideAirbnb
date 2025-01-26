531d89bfda47feea3f5084a03a71752399999bdf
Georg Tirpitz <georg.tirpitz@student.uni-tuebingen.de>
2025-01-26 17:55:03 +0100

    added shap analysis to regression models

--- models/regression.py
+++ models/regression.py
@@ -12,18 +12,13 @@ import numpy as np
 from scipy import stats
 import os
 from add_custom_features import AddCustomFeatures
+import shap
+import logging
+from tqdm import tqdm
 
+# Configure logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
-def model_specific_preprocess(data_df: pd.DataFrame, Feature_Selection: dict) -> Tuple:
-        """ Preprocess the data for the TabPFN model"""
-        # Ensure all features are numeric
-        data_df = data_df.dropna(subset=Feature_Selection['features'] + [Feature_Selection['target']])
-        X = data_df[Feature_Selection['features']]
-        y = data_df[Feature_Selection['target']]
-        X = X.fillna(X.mean())
-        # Z-score normalization
-        #X = (X - X.mean()) / X.std()
-        return X, y
 
 class RegressionModels:
     """ Fit, evaluate, and get attributions regression models (current: Random Forest and Linear Regression)"""
@@ -37,12 +32,13 @@ class RegressionModels:
             identifier: str = None,
             top_n: int = 10):
         
+        logging.info("Initializing RegressionModels class...")
         self.Feature_Selection = Feature_Selection
         self.top_n = top_n
         self.rf_hparams = rf_hparams
         self.linear_model = None
         self.rf_model = None
-        X,y = self.model_specific_preprocess(data_df)
+        X, y = self.model_specific_preprocess(data_df)
         self.train_split = train_test_split(X, y, test_size=test_split_size, random_state=42)
         self.save_path = save_path
         self.identifier_rf, self.identifier_linear = identifier
@@ -51,11 +47,12 @@ class RegressionModels:
 
         save_path = f'{save_path}' 
         if not os.path.exists(save_path):
-                os.makedirs(save_path)
-
+            os.makedirs(save_path)
+        logging.info("Finished initializing RegressionModels class.")
 
     def model_specific_preprocess(self, data_df: pd.DataFrame, Feature_Selection: dict = None) -> Tuple:
         """ Preprocess the data for the TabPFN model"""
+        logging.info("Starting model-specific preprocessing...")
         # Ensure all features are numeric
         if Feature_Selection is None:
             Feature_Selection = self.Feature_Selection
@@ -64,13 +61,16 @@ class RegressionModels:
         X = data_df[Feature_Selection['features']]
         y = data_df[Feature_Selection['target']]
         X = X.fillna(X.mean())
-        # Z-score normalization
-        #X = (X - X.mean()) / X.std()
+        X = X.apply(pd.to_numeric, errors='coerce')
+        # Remove dollar sign and convert to float
+        if y.dtype == object:
+            y = y.replace('[\$,]', '', regex=True).astype(float)
+        logging.info("Finished model-specific preprocessing.")
         return X, y
     
     def fit(self) -> None:
         """ Train and predict using Linear Regression and Random Forest"""
-    
+        logging.info("Starting model training...")
         X_train, X_test, y_train, y_test = self.train_split
 
         # Linear Regression
@@ -81,28 +81,29 @@ class RegressionModels:
 
         self.linear_model = linear_model
         self.rf_model = rf_model
+        logging.info("Finished model training.")
        
     def predict(self, X: pd.DataFrame , y: pd.DataFrame = None, save_results=False) -> Tuple:
         """ Predict using the trained models (for class extern usage)"""
+        logging.info("Starting prediction...")
         nan_counts = X.isna().sum()
-        print("NaN counts per column:")
-        print(nan_counts)
+        logging.info("NaN counts per column:")
+        logging.info(nan_counts)
         rf_pred = self.rf_model.predict(X)
         linear_pred = self.linear_model.predict(X)
         
-        
-        
-        if save_results == True:
+        if save_results:
             # Optionally save predictions
             results_df = pd.DataFrame({'y_test': y, 'y_pred': rf_pred})
             results_df.to_csv(f'{self.save_path}/{self.identifier_rf}_results.csv', index=False)
-        if save_results == True:
             results_df = pd.DataFrame({'y_test': y, 'y_pred':linear_pred})
             results_df.to_csv(f'{self.save_path}/{self.identifier_linear}_results.csv', index=False)
+        logging.info("Finished prediction.")
         return rf_pred, linear_pred
 
     def evaluate(self) -> Tuple:
         """ Evaluate the models using mean squared error, r2 score and cross validation"""
+        logging.info("Starting model evaluation...")
         X_train, X_test, y_train, y_test = self.train_split
 
         # Linear Regression
@@ -123,10 +124,10 @@ class RegressionModels:
         linear_cv_scores = cross_val_score(self.linear_model, X_train, y_train, cv=10, scoring='r2')
         linear_cv_mean_score = linear_cv_scores.mean()
 
-        print(f"Random Forest MSE: {rf_mse}, R2: {rf_r2}")
-        print(f"Linear Regression MSE: {linear_mse}, R2: {linear_r2}")
-        print(f"Random Forest 10-fold CV Mean R2 Score: {rf_cv_mean_score}")
-        print(f"Linear Regression 10-fold CV Mean R2 Score: {linear_cv_mean_score}")
+        logging.info(f"Random Forest MSE: {rf_mse}, R2: {rf_r2}")
+        logging.info(f"Linear Regression MSE: {linear_mse}, R2: {linear_r2}")
+        logging.info(f"Random Forest 10-fold CV Mean R2 Score: {rf_cv_mean_score}")
+        logging.info(f"Linear Regression 10-fold CV Mean R2 Score: {linear_cv_mean_score}")
 
         # Compute p-values for Linear Regression
         params = np.append(self.linear_model.intercept_, self.linear_model.coef_)
@@ -144,8 +145,8 @@ class RegressionModels:
             _ , p = stats.pearsonr(X_test.iloc[:, i], rf_pred)
             rf_p_values.append(p)
 
-        print(f"Random Forest p-values: {rf_p_values}")
-        print(f"Linear Regression p-values: {linear_p_values}")
+        logging.info(f"Random Forest p-values: {rf_p_values}")
+        logging.info(f"Linear Regression p-values: {linear_p_values}")
 
         linear_metrics = {
             'mse': linear_mse,
@@ -167,10 +168,12 @@ class RegressionModels:
 
         linear_metrics_df.to_csv(f'{self.save_path}/{self.identifier_linear}_metrics.csv', index=False)
         rf_metrics_df.to_csv(f'{self.save_path}/{self.identifier_rf}_metrics.csv', index=False)
+        logging.info("Finished model evaluation.")
         return linear_metrics, rf_metrics
     
     def feature_importance(self, top_n: int = 10, save_results = True) -> Tuple[Dict, Dict]:
         """ Return the feature importance for the Random Forest and linear model"""
+        logging.info("Starting feature importance evaluation...")
         # Get attributions
         rf_attribution = self.rf_model.feature_importances_
         linear_attribution = abs(self.linear_model.coef_) / sum(abs(self.linear_model.coef_))
@@ -185,16 +188,49 @@ class RegressionModels:
 
         if save_results:
             np.save(f'{self.save_path}/{self.identifier_rf}_feature_importance.npy', rf_top_features)
-        if save_results:
             np.save(f'{self.save_path}/{self.identifier_linear}_feature_importance.npy', linear_top_features)
         
         self.rf_importances = rf_top_features
         self.linear_importances = linear_top_features
 
+        logging.info("Starting SHAP importance evaluation...")
+
+        # Initialize SHAP
+        shap.initjs()
+
+        # Sample background data
+        X_train, X_test, y_train, y_test = self.train_split
+        background_data = X_train.sample(50, random_state=42)
+        logging.info("Background data for SHAP initialized.")
+
+        # SHAP for Random Forest
+        rf_explainer = shap.TreeExplainer(self.rf_model)
+        rf_shap_values = rf_explainer.shap_values(X_train)
+        logging.info("SHAP explainer for Random Forest initialized.")
+        shap.summary_plot(rf_shap_values, X_train, plot_type="bar", show=False)
+        plt.savefig(f'{self.save_path}/{self.identifier_rf}_shap_summary.png')
+        plt.close()
+        shap.summary_plot(rf_shap_values, X_train, show=False)
+        plt.savefig(f'{self.save_path}/{self.identifier_rf}_shap_beeswarm.png')
+        plt.close()
+
+        # SHAP for Linear Regression
+        linear_explainer = shap.LinearExplainer(self.linear_model, background_data)
+        linear_shap_values = linear_explainer.shap_values(X_train)
+        logging.info("SHAP explainer for Linear Regression initialized.")
+        shap.summary_plot(linear_shap_values, X_train, plot_type="bar", show=False)
+        plt.savefig(f'{self.save_path}/{self.identifier_linear}_shap_summary.png')
+        plt.close()
+        shap.summary_plot(linear_shap_values, X_train, show=False)
+        plt.savefig(f'{self.save_path}/{self.identifier_linear}_shap_beeswarm.png')
+        plt.close()
+
+        logging.info("Finished feature importance evaluation.")
         return rf_top_features, linear_top_features
 
     def plot(self) -> None:
         """ Plot """
+        logging.info("Starting plotting feature importances...")
         # Bar plot for Random Forest feature importances
         plt.figure(figsize=(10, 6))
         rf_features = list(self.rf_importances.keys())
@@ -216,15 +252,13 @@ class RegressionModels:
         plt.gca().invert_yaxis()
         plt.savefig(f'{self.save_path}/{self.identifier_linear}_feature_importances.png')
         plt.close()
-
-
-        
+        logging.info("Finished plotting feature importances.")
 
 if __name__ == "__main__":
-    #folder_path = "/Users/georgtirpitz/Documents/Data_Literacy"
+    logging.info("Starting main execution...")
     folder_path = "/home/georg/Documents/Master/Data_Literacy"
     data_df = pd.read_csv(folder_path + "/city_listings.csv")
-    safe_path = folder_path + "/results" + "test"
+    safe_path = folder_path + "/DataLit-InsideAirbnb" + "/results" + "/regressions"
     identifier = ["RandomForest", "LinearRegression"]
     # Setting features and target
     Feature_Selection = {
@@ -252,7 +286,7 @@ if __name__ == "__main__":
     }
     n_top_features = 15 # Number of top features to show
 
-    model = model = RegressionModels(
+    model = RegressionModels(
         data_df, 
         RandomForest_Hparams, 
         Feature_Selection, 
@@ -269,4 +303,5 @@ if __name__ == "__main__":
     metrics = model.evaluate()
     importances = model.feature_importance(10)
     model.plot()
-    print("Finished")
+    logging.info("Finished main execution.")
+    
